7e42cc6bbb1ae190dc7245e588efce261d8e510b
gmngorup <gureshidze1994@gmail.com>
2025-04-08 11:44:51 +0400

    Map.tsx

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -5,8 +5,8 @@ interface MapProps {
 function Map({ isEnglish }: MapProps) {
   // Define the base Google Maps iframe URL with placeholders for the language
   const mapUrl = isEnglish
-    ? "https://www.google.com/maps/embed/v1/place?q=ლ.ადეიშვილის+თვალის+კლინიკა,+4+Bukhaidze+Street,+Kutaisi+4600&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&language=en"
-    : "https://www.google.com/maps/embed/v1/place?q=ლ.ადეიშვილის+თვალის+კლინიკა,+4+Bukhaidze+Street,+Kutaisi+4600&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&language=ka";
+    ? "https://www.google.pl/maps/@/data=!3m1!4b1!4m3!11m2!2soDLaCnl1Qz2QaR-4HzZR-g!3e3?coh=245189&entry=tts&g_ep=EgoyMDI1MDQwMi4xKgksMTAyMTE0NTVIAVAD&skid=eb90c4a1-9daf-4c7a-bc5f-2ad2515e6ed1"
+    : "https://www.google.pl/maps/@/data=!3m1!4b1!4m3!11m2!2soDLaCnl1Qz2QaR-4HzZR-g!3e3?coh=245189&entry=tts&g_ep=EgoyMDI1MDQwMi4xKgksMTAyMTE0NTVIAVAD&skid=eb90c4a1-9daf-4c7a-bc5f-2ad2515e6ed1";
 
   return (
     <div className="w-full md:h-[500px] h-[320px]">
